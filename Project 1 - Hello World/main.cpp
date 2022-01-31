c7f7696a0ab63acab7d299532bcb7333536202d3
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-31 09:44:38 -0700

    Hello World Project

--- Project 1 - Hello World/main.cpp
+++ Project 1 - Hello World/main.cpp
@@ -1,28 +1,32 @@
-#include<iostream>
+#include <iostream>
 #include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
 
 /*
-* This is an implementation of an animated background windows using FreeGlut and OpenGL.
-* Author: Pranav Rajan
-* Version: January 21, 2022
-*/
+ * This is an implementation of an animated background windows using FreeGlut and OpenGL.
+ * Author: Pranav Rajan
+ * Version: January 21, 2022
+ */
 
 // global variable for incrementing the red color value
 float red = 0.0;
-void myIdle() {
-	// Example Animation 
-	//std::cout << "red " << red << std::endl;
+void myIdle()
+{
+	// Example Animation
+	// std::cout << "red " << red << std::endl;
 	glClearColor(red, 0, 0, 0);
 	red += 0.02;
 	Sleep(100);
-	if (red > 1.0) {
+	if (red > 1.0)
+	{
 		red = 0.0;
 	}
 
 	glutPostRedisplay();
 }
 
-void myDisplay() {
+void myDisplay()
+{
 	// Clear the viewport
 	glClear(GL_COLOR_BUFFER_BIT);
 
@@ -30,13 +34,14 @@ void myDisplay() {
 
 	// Swap buffers
 	glutSwapBuffers();
-
 }
 
-void myKeyboard(unsigned char key, int x, int y) {
+void myKeyboard(unsigned char key, int x, int y)
+{
 	// Handle keyboard input here
-	// GLUT uses ascii for key events 
-	switch (key) {
+	// GLUT uses ascii for key events
+	switch (key)
+	{
 	case 27:
 		glutLeaveMainLoop();
 		break;
@@ -44,11 +49,12 @@ void myKeyboard(unsigned char key, int x, int y) {
 	return;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	// GLUT Initialization
 	glutInit(&argc, argv);
 
-	// Create a window 
+	// Create a window
 	glutInitWindowSize(1920, 1080);
 	glutInitWindowPosition(100, 100);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
@@ -57,13 +63,13 @@ int main(int argc, char** argv) {
 	// OpenGL Initialization
 	glClearColor(0, 0, 0, 0); // RGBA Values between 0-1
 
-	// Register display callback function 
+	// Register display callback function
 	glutDisplayFunc(myDisplay);
 
 	// Register special keyboard callback
 	glutKeyboardFunc(myKeyboard);
 
-	// Register Idle Function 
+	// Register Idle Function
 	glutIdleFunc(myIdle);
 
 	// Create buffers
@@ -73,5 +79,5 @@ int main(int argc, char** argv) {
 	// Other Initialization
 
 	glutMainLoop(); // Call main loop
-	return 0; // Exit when main loop is done 
+	return 0;		// Exit when main loop is done
 }
